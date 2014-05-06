d5733d9df9d30b6e19f99672c8bc2d2aad53d35f
Vivek Pandya <vivekvpandya@gmail.com>
2014-05-06 11:30:34 +0530

    Temp try to solve issue one on github “ loadHTMLString method causes app to crash if passed with nil or empty object.

--- TipsAndTricksCFNetwork/TnTViewController.m
+++ TipsAndTricksCFNetwork/TnTViewController.m
@@ -28,8 +28,38 @@
     [super viewDidLoad];
 
     if (self.tip) {
-        self.titleLable.text = [self.tip objectForKey:@"title"];
-        [self.bodyWebView loadHTMLString:[self.tip objectForKey:@"body"] baseURL:nil];
+   
+        NSString *titleString = [[NSString alloc]initWithFormat:@"%@",[self.tip objectForKey:@"title"]];
+        
+        //as per my site configuration all fields are required but it may be the case that still we don't get title or body of tip than we should handle that
+        
+        // JSON object returns <null> string if any of the filed is empty.
+        
+        
+        if (titleString != nil && ![titleString isEqualToString:@"<null>"]) {
+            self.titleLable.text = titleString;
+        }
+        else{
+            
+            UIAlertView *errorAlert = [[UIAlertView alloc]initWithTitle:@"Error" message:@"Cound not load tip title or It may be empty" delegate:nil cancelButtonTitle:@"OK" otherButtonTitles:nil];
+            [errorAlert show];
+
+        
+        }
+        NSString *bodyHTML = [[NSString alloc]initWithFormat:@"%@",[self.tip objectForKey:@"body"]];
+        if (bodyHTML != nil && ![bodyHTML isEqualToString:@"<null>"]) {
+            
+            [self.bodyWebView loadHTMLString:bodyHTML baseURL:nil];
+                    }
+        else {
+        
+            UIAlertView *errorAlert = [[UIAlertView alloc]initWithTitle:@"Error" message:@"Cound not load tip content or It is empty" delegate:nil cancelButtonTitle:@"OK" otherButtonTitles:nil];
+            [errorAlert show];
+        }
+    }
+    else{
+    UIAlertView *errorAlert = [[UIAlertView alloc]initWithTitle:@"Error" message:@"Cound not load tip data" delegate:nil cancelButtonTitle:@"OK" otherButtonTitles:nil];
+    [errorAlert show];
     }
     // Do any additional setup after loading the view.
 }
